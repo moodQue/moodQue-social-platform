a2deb824972ef1fda2099776a2919e231f53715a
moodQue Social <greatdayteesstore@gmail.com>
2025-07-31 10:59:25 -0400

    Removed Get Similar Tracks

--- moodque_engine.py
+++ moodque_engine.py
@@ -12,7 +12,7 @@ def get_user_id_from_spotify_id(spotify_user_id):
 
     return None
 
-from lastfm_recommender import get_recommendations, get_similar_artists, get_similar_tracks, search_tracks_by_artist, get_genre_seed_artists
+from lastfm_recommender import get_recommendations, get_similar_artists, search_tracks_by_artist, get_genre_seed_artists
 
 import os
 import requests
